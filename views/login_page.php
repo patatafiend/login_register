d7a55d5db1d0c08bef281094d19352ceab1ce8ef
Renz Trimidal <trimidal42@gmail.com>
2025-02-13 18:31:48 +0800

    removes and adds fade-in effect

--- views/login_page.php
+++ views/login_page.php
@@ -49,7 +49,7 @@
         let formContainer = document.querySelector(".col-md-4");
         if (formContainer) {
             formContainer.classList.remove("fade-in");
-            void formContainer.offsetWidth; // Trigger reflow
+            void formContainer.offsetWidth;
             formContainer.classList.add("fade-in");
         }
     });
