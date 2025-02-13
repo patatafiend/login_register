632442b1d9dee5f517a3ac8018536bfa07972eaf
Renz Trimidal <trimidal42@gmail.com>
2025-02-13 18:25:23 +0800

    fixed logout not destroying session

--- logout.php
+++ logout.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-$_SESSION["loggedin"] = false;
-session_unset();
 session_destroy();
-
-header("Cache-Control: no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: Sat, 1 Jan 2000 00:00:00 GMT");
 
 
 header('Location: /login_register/views/login_page.php');
